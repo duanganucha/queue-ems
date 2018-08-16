40954e174f0c47101051f262ba719d48e53e2a18
duanganucha <duanganucha@hotmail.com>
2018-08-17 04:20:34 +0700

    last

---
+++ src/app/right/right.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { AngularFireDatabase,AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+  selector: 'app-right',
+  templateUrl: './right.component.html',
+  styleUrls: ['./right.component.scss']
+})
+
+export class RightComponent  {
+
+  itemsRef: AngularFireList<any>;
+  items: Observable<any[]>;
+  
+  item : Items = new Items(); 
+  dateInput? : string;
+  
+  constructor(private afDB: AngularFireDatabase) {
+    this.initialData()
+  }
+
+  initialData(){
+    this.itemsRef = this.afDB.list(`queue/${this.dateInput}`);
+    this.items = this.itemsRef.snapshotChanges().map(changes => {
+      return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    });
+  }
+
+  inputDateFn(){
+    console.log(this.dateInput);
+    this.initialData();
+  }
+
+
+
+  onManager(item){
+    console.log(item.key)
+    this.item.timestamp = item.timestamp;
+    this.item.number = item.number;
+    this.item.team = item.team;
+    this.item.scene = item.scene;
+    this.item.user = item.user;
+    this.item.note = item.note;
+    this.item.key = item.key;
+  }
+
+  onUpdate(){
+    console.log(this.item.team)
+    this.itemsRef = this.afDB.list(`queue/${this.dateInput}`);
+    this.itemsRef.update(this.item.key , { 
+      team : this.item.team ,
+      scene : this.item.scene,
+      note : this.item.note
+    })
+  }
+
+}
+
+class Items  {
+  key : string;
+  number : number;
+  timestamp2 = Date.now();
+  timestamp ;
+  team:string;
+  scene:string;
+  user:string;
+  note:string;
+
+}
